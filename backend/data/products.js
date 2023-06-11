9eced19576657fc74b32730eea3779664268fd51
BhanuPratapsSngh <102852943+BhanuPratapsSngh@users.noreply.github.com>
2023-06-12 00:27:09 +0530

    Add files via upload

---
+++ backend/data/products.js
@@ -0,0 +1,75 @@
+const products = [
+  {
+    name: 'Jacket',
+    image: '/images/jacket.jpg',
+    description:'jacket blue color',
+      
+    brand: 'Roadster',
+    category: 'cloths',
+    price: 2500,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: 'MEN HANDCRAFTED BROWN FORMAL LACEUP SHOES',
+    image: '/images/formalshoe.jpg',
+    description:'Oxford glossy brown formal laceup , perfect for every outings . Handcrafted made for longer durability and usage with most basic and traditional shoe shape is used round toe which makes the shoes classic and modified with the trendiest designs ',
+    brand: 'LongWalk',
+    category: 'shoe',
+    price: 1500,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Watch for Men',
+    image: '/images/watch.jpg',
+    description:
+      'black coloured smart watch for men',
+    brand: 'Casio Quartz',
+    category: 'accessories ',
+    price: 899,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: 'Jacket ',
+    image: '/images/Jacket2.jpg',
+    description:
+    'Winter wear for men, light-green jacket for men, woollen jacket.',
+    brand: 'woodland',
+    category: 'cloths',
+    price: 2999,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Winter wear for women',
+    image: '/images/winterw.jpg',
+    description:
+      'Blue coloured woollen sweater for women',
+    brand: 'Tommy Hilfiger',
+    category: 'cloths',
+    price: 2449,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Hoodie for Men',
+    image: '/images/hoodie.jpg',
+    description:
+      'double-coloured casual hoodie wear for men',
+    brand: 'Puma',
+    category: 'cloths',
+    price: 1999,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
